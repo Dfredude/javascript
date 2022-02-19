24043add778483efcc8f6ad5a8a88e17e932125f
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-02-18 17:21:07 -0800

    chapter-2; 7

---
+++ chapter-02/07-Array.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for (i in arr){
+    if (i + 1 === 10) console.log(a[i]);
+}
+console.log(`
+It doesn't print anything to the console because the 'i' is the key value of the Array, which is an object.
+Comparing strictly string to number will never turn out to True
+`);
